2c4e6da2a0f44b376104b0df4e7fdd52f41c9629
Ankit Mathur <ankitmathur@berkeley.edu>
2018-12-10 18:11:59 -0800

    training scene id

--- scene_identifier.py
+++ scene_identifier.py
@@ -8,20 +8,20 @@ TRAIN_EPOCHS = 100
 scene_to_label = {1:0, 3:1, 4:2, 5:3}
 # Input Specification
 def parse_fn(f, l):
-	image_string = tf.read_file(f)
-	image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-	image_resized = tf.image.resize_images(image_decoded, (299,299))
-	image = tf.cast(image_resized, tf.float32)
-	label = tf.one_hot(l, depth=len(scenes))
-	print(label)
-	return image, label
+    image_string = tf.read_file(f)
+    image_decoded = tf.image.decode_jpeg(image_string, channels=3)
+    image_resized = tf.image.resize_images(image_decoded, (299,299))
+    image = tf.cast(image_resized, tf.float32)
+    label = tf.one_hot(l, depth=len(scenes))
+    print(label)
+    return image, label
 images, labels = [], []
 for scene_idx in scenes:
-	images_from_scene = get_all_images_for_scene(scene_idx)
-	# images_from_scene = map(parse_fn, images_from_scene)
-	images.extend(images_from_scene)
-	labels.extend([scene_to_label[scene_idx] for _ in range(len(images_from_scene))])
-	# sess.run([images])
+    images_from_scene = get_all_images_for_scene(scene_idx)
+    # images_from_scene = map(parse_fn, images_from_scene)
+    images.extend(images_from_scene)
+    labels.extend([scene_to_label[scene_idx] for _ in range(len(images_from_scene))])
+    # sess.run([images])
 def train_input_fn(features, labels, batch_size):
     """An input function for training"""
     # Convert the inputs to a Dataset.
@@ -35,49 +35,50 @@ def train_input_fn(features, labels, batch_size):
 # Model Specification
 
 def create_model(inputs):
-	# inputs = tf.placeholder(tf.float32, (None, 299, 299, 3))
-	conv_outs = tf.layers.conv2d(inputs, 16, kernel_size=5)
-	flattened = tf.contrib.layers.flatten(conv_outs)
-	fc_outs = tf.layers.dense(flattened, 1024)
-	fc_final = tf.layers.dense(fc_outs, 4)
-	return fc_final
+    # inputs = tf.placeholder(tf.float32, (None, 299, 299, 3))
+    conv_outs = tf.layers.conv2d(inputs, 16, kernel_size=5)
+    flattened = tf.contrib.layers.flatten(conv_outs)
+    fc_outs = tf.layers.dense(flattened, 1024)
+    fc_final = tf.layers.dense(fc_outs, 4)
+    return fc_final
 
 def model_fn(features, labels, mode, params):
-	print(features)
-	images = features
-	model = create_model(images)
-	if mode == tf.estimator.ModeKeys.PREDICT:
-		logits = model
-		predictions = {
-			'classes': tf.argmax(logits, axis=1),
-			'probabilities': tf.nn.softmax(logits)
-		}
-		return tf.estimator.EstimatorSpec(mode=tf.estimator.ModeKeys.PREDICT,predictions=predictions)
-	elif mode == tf.estimator.ModeKeys.TRAIN:
-		optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
-		logits = model
-		loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits)
-		# accuracy = tf.metrics.accuracy(labels=labels, predictions=tf.argmax(logits))
-		tf.identity(LEARNING_RATE, 'learning_rate')
-		tf.identity(loss, 'cross_entropy')
-		# tf.identity(accuracy[1], name='train_accuracy')
-		# tf.summary.scalar('train_accuracy', accuracy[1])
-		return tf.estimator.EstimatorSpec(mode=tf.estimator.ModeKeys.TRAIN, loss=loss, train_op=optimizer.minimize(loss, tf.train.get_or_create_global_step()))
-	elif mode == tf.estimator.ModeKeys.EVAL:
-		logits = model
-		loss = tf.losses.softmax_cross_entropy(labels=labels, logits=logits)
-		return tf.estimator.EstimatorSpec(mode=tf.estimator.ModeKeys.EVAL, loss=loss, eval_metric_ops={'accuracy' : tf.metrics.accuracy(labels=labels, predictions=tf.argmax(logits, axis=1))})
+    print(features)
+    images = features
+    model = create_model(images)
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        logits = model
+        predictions = {
+            'classes': tf.argmax(logits, axis=1),
+            'probabilities': tf.nn.softmax(logits)
+        }
+        return tf.estimator.EstimatorSpec(mode=tf.estimator.ModeKeys.PREDICT,predictions=predictions)
+    elif mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE)
+        logits = model
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=logits)
+        print(tf.argmax(logits, axis=1))
+        print(labels)
+        # accuracy = tf.metrics.accuracy(labels=labels, predictions=tf.argmax(logits, axis=1))
+        tf.identity(LEARNING_RATE, 'learning_rate')
+        tf.identity(loss, 'cross_entropy')
+        tf.summary.scalar('xentropy_loss', loss)
+        # tf.identity(accuracy[1], name='train_accuracy')
+        # tf.summary.scalar('train_accuracy', accuracy[1])
+        return tf.estimator.EstimatorSpec(mode=tf.estimator.ModeKeys.TRAIN, loss=loss, train_op=optimizer.minimize(loss, tf.train.get_or_create_global_step()))
+    elif mode == tf.estimator.ModeKeys.EVAL:
+        logits = model
+        loss = tf.losses.softmax_cross_entropy(labels=labels, logits=logits)
+        return tf.estimator.EstimatorSpec(mode=tf.estimator.ModeKeys.EVAL, loss=loss, eval_metric_ops={'accuracy' : tf.metrics.accuracy(labels=labels, predictions=tf.argmax(logits, axis=1))})
 
 def train_detector():
-	scene_classifier = tf.estimator.Estimator(
-      model_fn=model_fn,
-      model_dir=MODEL_DIR)
+    scene_classifier = tf.estimator.Estimator(model_fn=model_fn, model_dir=MODEL_DIR)
 
-	for i in range(TRAIN_EPOCHS):
-		print(i)
-		scene_classifier.train(input_fn=lambda: train_input_fn(images, labels, BATCH_SIZE))
-		# eval_results = scene_classifier.evaluate(input_fn=eval_input_fn)
-		print('\nEvaluation results:\n\t%s\n' % eval_results)
+    for i in range(TRAIN_EPOCHS):
+        print(i)
+        scene_classifier.train(input_fn=lambda: train_input_fn(images, labels, BATCH_SIZE))
+        # eval_results = scene_classifier.evaluate(input_fn=eval_input_fn)
+        print('\nEvaluation results:\n\t%s\n' % eval_results)
 train_detector()
-	
+    
 # load images
